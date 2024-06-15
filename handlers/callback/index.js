ee19ab82dd9b5d5e5121273df9f1c0de3ecb0935
miwist <miwist@mail.ru>
2024-06-15 20:31:31 +0600

    added jimp for xy3 image array

--- handlers/callback/index.js
+++ handlers/callback/index.js
@@ -1,4 +1,3 @@
-const { presend_text } = require("../../assets/text");
 const { bot } = require("../../connection");
 const fs = require("fs");
 
@@ -8,9 +7,7 @@ const botCallback = bot.on("callback_query", async (msg, match) => {
   const msgId = msg.message.message_id;
 
   try {
-    // ## В случае чего - можно настроить на кнопки
     if (data == "send_photo") {
-      bot.sendMessage(chatId, presend_text);
     }
   } catch (e) {
     fs.appendFile("errors.txt", `${e}` + "\n", function (e) {});
--- handlers/functions/checkWeld.js
+++ handlers/functions/checkWeld.js
@@ -3,13 +3,46 @@ const { bot, pool, isDate } = require("../../connection");
 require("dotenv").config();
 const fs = require("fs");
 const { error_text, undefined_check } = require("../../assets/text");
+const Jimp = require("jimp");
+
+async function get_image_array(image_path) {
+  try {
+    const image = await Jimp.read(image_path);
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    const data = image.bitmap.data;
+
+    const image_array = new Array(height);
+    for (let y = 0; y < height; y++) {
+      image_array[y] = new Array(width);
+
+      for (let x = 0; x < width; x++) {
+        const index = (y * width + x) * 4;
+
+        image_array[y][x] = [
+          data[index],
+          data[index + 1],
+          data[index + 2], 
+        ];
+      }
+    }
+
+    return image_array;
+  } catch (error) {
+    console.error("Ошибка при обработке изображения:", error);
+    return null;
+  }
+}
 
 const check_photo = (chatId, photo) => {
   let date_now = isDate();
 
+  let image_array = get_image_array(photo).then((image_array) => {
+    console.log(image_array);
+  });
+
   const data = {
-    url_photo: photo,
-    date: date_now,
+    url_photo: image_array,
   };
 
   const params = new URLSearchParams(data);
